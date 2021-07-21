30b3b07e120143e9cb8953827ea5a44867fb5f70
mozein15 <mozein15@gmail.com>
2021-07-21 03:57:28 -0700

    Hiding bottom chevron on mobile

--- app.js
+++ app.js
@@ -23,6 +23,13 @@
 //     });
 // }
 
+function getStyle(id, name) {
+    var element = document.getElementById(id);
+    return element.currentStyle ? element.currentStyle[name] : window.getComputedStyle ? window.getComputedStyle(element, null).getPropertyValue(name) : null;
+}
+
+const downchevron = document.getElementById('down-indicator');
+
 function scrollToId(eltId){
     const elmntToView = document.getElementById(eltId);
     console.log(elmntToView.offsetTop);
@@ -30,4 +37,15 @@ function scrollToId(eltId){
         elmntToView.scrollIntoView()
     },2);
     //elmntToView.scrollTo(0, elmntToView.offsetTop);
+}
+
+
+function hideChevronOnNavExpand(){
+    if(window.screen.width < 768) { // only on mobile
+        console.log(getStyle('down-indicator', 'visibility'));
+        if (getStyle('down-indicator', 'visibility') == 'visible')
+            downchevron.style.visibility = 'hidden';
+        else
+            downchevron.style.visibility = 'visible';
+    }
 }
