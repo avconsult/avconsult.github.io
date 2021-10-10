611672ba84530a45970598f91303ffe538c378f6
mozein15 <mozein15@gmail.com>
2021-10-10 03:48:01 -0700

    Animated signage page

--- jsapps/intersection-observer-signage.js
+++ jsapps/intersection-observer-signage.js
@@ -3,16 +3,20 @@ const firstCol = document.getElementById('firstCol');
 const secondCol = document.getElementById('secondCol');
 const thirdCol = document.getElementById('thirdCol');
 
-const options = {}; // same as root: null (viewport), threshold and rootMargin can also be specified
+const options = {threshold: 0.7}; // same as root: null (viewport), threshold and rootMargin can also be specified
 const observer = new IntersectionObserver(function(entries, observer){
     entries.forEach( entry => {
         if(entry.isIntersecting) {
-            if(entry.target.id == 'firstCol')
+            document.getElementById(entry.target.id).style.visibility = 'visible';
+            if(entry.target.id == 'firstCol') {
                 entry.target.classList.add("animate__backInLeft");
-            else if (entry.target.id == 'secondCol')
+            }
+            else if (entry.target.id == 'secondCol') {
                 entry.target.classList.add("animate__backInUp");
-            else
+            }
+            else {
                 entry.target.classList.add("animate__backInRight");
+            }
             observer.unobserve(entry.target);
         }
     });
